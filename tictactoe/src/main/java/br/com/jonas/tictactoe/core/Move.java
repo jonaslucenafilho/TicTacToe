a59db01c5d3cc4c23c41126963f4ab11a4eb146a
Jonas Lucena <jonaslucena@outlook.com>
2020-05-24 16:08:35 -0300

    implementation of constructor with parameter and methods to split string

--- tictactoe/src/main/java/br/com/jonas/tictactoe/core/Move.java
+++ tictactoe/src/main/java/br/com/jonas/tictactoe/core/Move.java
@@ -1,6 +1,15 @@
 package br.com.jonas.tictactoe.core;
 
 public class Move {
+	
+	public Move(String moveString) {
+		String[] tokens = moveString.split(",");
+		
+		this.i = Integer.parseInt(tokens[0]);
+		this.j = Integer.parseInt(tokens[1]);
+		
+		// TODO Validar se a estrutura do moveString está correta
+	}
 
 	private int i;
 	private int j;
