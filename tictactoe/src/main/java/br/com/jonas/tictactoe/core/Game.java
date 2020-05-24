c86d940a9b54948b3928e1d09bbf336a09d5d72f
Jonas Lucena <jonaslucena@outlook.com>
2020-05-24 16:43:18 -0300

    creation and implementation of method nextPlayer

--- tictactoe/src/main/java/br/com/jonas/tictactoe/core/Game.java
+++ tictactoe/src/main/java/br/com/jonas/tictactoe/core/Game.java
@@ -7,6 +7,7 @@ public class Game {
 
 	private Board board =  new Board();
 	private Player[] players = new Player[Constants.SYMBOL_PLAYERS.length];
+	private int currentPlayerIndex = 0;
 	
 	public void play() {
 		UI.printGameTitle();
@@ -24,5 +25,10 @@ public class Game {
 		UI.printText("O jogador '" + name + "' vai usar o símbolo '" + symbol + "'" );
 		
 		return player;
+	}
+	
+	private Player nextPlayer() {
+		currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+		return players[currentPlayerIndex];
 	}
 }
