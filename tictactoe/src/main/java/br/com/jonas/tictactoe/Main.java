7e38f4a89d6436ae1faecf69a8edef6268be66b4
Jonas Lucena <jonaslucena@outlook.com>
2020-05-24 16:31:25 -0300

    creation of main class and beginning implementation

---
+++ tictactoe/src/main/java/br/com/jonas/tictactoe/Main.java
@@ -0,0 +1,12 @@
+package br.com.jonas.tictactoe;
+
+import br.com.jonas.tictactoe.core.Game;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Game game = new Game();
+		game.play();
+	}
+}
