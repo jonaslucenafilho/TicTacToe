67db25bb5edaf8f054b3129a9db1b54bdba4dd45
Jonas Lucena <jonaslucena@outlook.com>
2020-05-24 16:30:32 -0300

    implementation of play and createPlayer methods

--- tictactoe/src/main/java/br/com/jonas/tictactoe/core/Game.java
+++ tictactoe/src/main/java/br/com/jonas/tictactoe/core/Game.java
@@ -9,6 +9,20 @@ public class Game {
 	private Player[] players = new Player[Constants.SYMBOL_PLAYERS.length];
 	
 	public void play() {
+		UI.printGameTitle();
 		
+		for (int i = 0; i < players.length; i++) {
+			players[i] = createPlayer(i);
+		}
+	}
+	
+	private Player createPlayer(int index) {
+		String name = UI.readInput("Jogador " + (index + 1) + " ->");
+		char symbol = Constants.SYMBOL_PLAYERS[index];
+		Player player = new Player(name, board, symbol);
+		
+		UI.printText("O jogador '" + name + "' vai usar o símbolo '" + symbol + "'" );
+		
+		return player;
 	}
 }
