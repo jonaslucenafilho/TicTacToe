09b354449cdd095004a6a64e7e51ce49c0e44789
Jonas Lucena <jonaslucena@outlook.com>
2020-05-24 15:19:18 -0300

    creation of board class

---
+++ tictactoe/src/main/java/br/com/jonas/tictactoe/core/Board.java
@@ -0,0 +1,25 @@
+package br.com.jonas.tictactoe.core;
+
+public class Board {
+	
+	private char[][] matrix;
+	
+	public Board() {
+	}
+	
+	public void clear() {
+
+	}
+	
+	public void print() {
+		
+	}
+	
+	public boolean isFull() {
+		return true;
+	}
+	
+	public boolean play() {
+		return false;
+	}
+}
