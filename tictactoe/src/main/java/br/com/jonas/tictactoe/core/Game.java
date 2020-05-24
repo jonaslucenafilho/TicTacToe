246280c34ef5eb51b8d2dbd380446707ba4c66c7
Jonas Lucena <jonaslucena@outlook.com>
2020-05-24 15:18:24 -0300

    creation of game class

---
+++ tictactoe/src/main/java/br/com/jonas/tictactoe/core/Game.java
@@ -0,0 +1,14 @@
+package br.com.jonas.tictactoe.core;
+
+import br.com.jonas.tictactoe.Constants;
+import br.com.jonas.tictactoe.ui.UI;
+
+public class Game {
+
+	private Board board =  new Board();
+	private Player[] players = new Player[Constants.SYMBOL_PLAYERS.length];
+	
+	public void play() {
+		
+	}
+}
