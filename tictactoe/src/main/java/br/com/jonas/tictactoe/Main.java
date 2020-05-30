3e48880be4912e12a48602be7a3b6457f5ce42db
Jonas Lucena <jonaslucena@outlook.com>
2020-05-30 18:09:43 -0300

    add throws exception in Main

--- tictactoe/src/main/java/br/com/jonas/tictactoe/Main.java
+++ tictactoe/src/main/java/br/com/jonas/tictactoe/Main.java
@@ -1,10 +1,12 @@
 package br.com.jonas.tictactoe;
 
+import java.io.IOException;
+
 import br.com.jonas.tictactoe.core.Game;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Game game = new Game();
 		game.play();
