da702d09ee23395a3e0a82d7cdf2adb07bb8f911
Jonas Lucena <jonaslucena@outlook.com>
2020-05-30 18:06:45 -0300

    creation and implementation of the ScoreManager interface

---
+++ tictactoe/src/main/java/br/com/jonas/tictactoe/score/ScoreManager.java
@@ -0,0 +1,12 @@
+package br.com.jonas.tictactoe.score;
+
+import java.io.IOException;
+
+import br.com.jonas.tictactoe.core.Player;
+
+public interface ScoreManager {
+
+	public Integer getScore(Player player);
+	
+	public void saveScore(Player player) throws IOException;
+}
